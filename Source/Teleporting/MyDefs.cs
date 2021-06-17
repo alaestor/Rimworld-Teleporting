bce7f9eac87394c61b8effa709a9414a52e5211d
Alaestor <alaestor@null.net>
2021-06-17 15:19:10 -0400

    MyDefs

---
+++ Source/Teleporting/MyDefs.cs
@@ -0,0 +1,10 @@
+﻿using Verse;
+
+namespace alaestor_teleporting
+{
+	class MyDefs
+	{
+		public static readonly ThingDef teleportConsole = ThingDef.Named("alaestor_teleporting_TeleportConsole");
+		public static readonly ThingDef teleportCartridge = ThingDef.Named("alaestor_teleporting_TeleportCartridge");
+	}
+}
