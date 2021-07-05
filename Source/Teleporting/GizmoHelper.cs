f6aba7eb0e869ca507d94913a69fd2e8f2e383db
Alaestor <alaestor@null.net>
2021-07-05 07:21:33 -0400

    made GimzoHelper utility

---
+++ Source/Teleporting/GizmoHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using Verse;
+
+namespace alaestor_teleporting
+{
+	class GizmoHelper
+	{
+		public static Command_Action MakeCommandAction(
+			string name,
+			Action action,
+			SoundDef activateSound = null,
+			Texture2D icon = null,
+			bool disabled = false,
+			KeyBindingDef hotKey = null)
+		{
+			return new Command_Action
+			{
+				defaultLabel = (name + "_Label").Translate(),
+				defaultDesc = (name + "_Desc").Translate(),
+				activateSound = activateSound ?? SoundDef.Named("Click"),
+				hotKey = hotKey,
+				icon = icon,
+				disabled = disabled,
+				disabledReason = (disabled ? (name + "_DisabledReason").Translate() : null),
+				action = action
+			};
+		}
+	}
+}
