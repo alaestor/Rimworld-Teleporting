0f29b9bf15e3a1f3adc3eeaff199fb2cfc7ff636
Alaestor <alaestor@null.net>
2021-07-10 10:41:15 -0400

    issue #6 research defs

--- Source/Teleporting/MyDefs.cs
+++ Source/Teleporting/MyDefs.cs
@@ -14,6 +14,11 @@ namespace alaestor_teleporting
 		public static readonly ThingDef apparel_TeleportBelt_Link = ThingDef.Named("alaestor_teleporting_Apparel_TeleportBelt_Link");
 		public static readonly ThingDef portableTeleportUnit_Local = ThingDef.Named("alaestor_teleporting_PortableTeleportUnit_Local");
 		public static readonly ThingDef portableTeleportUnit_Global = ThingDef.Named("alaestor_teleporting_PortableTeleportUnit_Global");
+
+		public static readonly ResearchProjectDef research_tier_1_0 = ResearchProjectDef.Named("alaestor_teleporting_Research_tier_1_0");
+		public static readonly ResearchProjectDef research_tier_2_0 = ResearchProjectDef.Named("alaestor_teleporting_Research_tier_2_0");
+		public static readonly ResearchProjectDef research_tier_3_0 = ResearchProjectDef.Named("alaestor_teleporting_Research_tier_3_0");
+		public static readonly ResearchProjectDef research_tier_4_0 = ResearchProjectDef.Named("alaestor_teleporting_Research_tier_4_0");
 	}
 
 	[DefOf]
